4e25a4ccc6a1e12262f7239abf7895289a59d20f
Seirra <sophietheopossum@yandex.ru>
2024-12-20 00:28:56 +0000

    add create file

---
+++ create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE data (
+  brand TEXT NOT NULL,
+  type TEXT NOT NULL,
+  sulfate TEXT NOT NULL,
+  dmdmhydantoin TEXT NOT NULL,
+  ml INTEGER(1),
+  pack INTEGER(1),
+  cost REAL(2,2),
+  mlCost REAL(1,2),
+  urla TEXT,
+  urlb TEXT,
+  urlc TEXT
+);
+UPDATE data SET mlCost = cost / (ml * pack);
+
+SELECT * from data ORDER BY mlCost;
