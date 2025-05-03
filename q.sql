93fccb920a78ae8f68a765ce133bcac80964e6fb
Seirra <sophietheopossum@yandex.ru>
2025-05-04 00:16:03 +0100

    Improves mlCost calculation and filtering
    Updates the mlCost calculation to include a discount multiplier.
    
    Filters out zero mlCost values to prevent division by zero errors and improve result accuracy.

--- q.sql
+++ q.sql
@@ -1,3 +1,3 @@
-UPDATE data SET mlCost = cost / (ml * pack);
+UPDATE data SET mlCost = (cost * discmult) / (ml * pack);
 
-SELECT * from data WHERE mlCost IS NOT NULL ORDER BY mlCost;
+SELECT * from data WHERE mlCost IS NOT NULL AND mlCost IS NOT 0 ORDER BY mlCost;
